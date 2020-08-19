6a626e6fe9b58d75be387bde276fed4131c153cf
tc91043 <69646404+tc91043@users.noreply.github.com>
2020-08-19 12:02:06 -0700

    Add files via upload

---
+++ Gunboat.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+ 
+	Scanner reader = new Scanner(System.in);
+	
+	System.out.println("Enter distance to target.\n");
+	double d = reader.nextDouble();
+	System.out.println("Enter height of battery relative to water line.\n");
+	double h = reader.nextDouble();
+	System.out.println("Enter intial velocity of shell.\n");
+	double v = reader.nextDouble();
+	
+	double a = -4.9*(d/v)*(d/v);
+	double b = d;
+	double c = h + a;
+	
+	double tanTheta = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
+	double angle = Math.atan(tanTheta)*180/Math.PI;
+	System.out.println(angle + " degrees");
+    }
+}
